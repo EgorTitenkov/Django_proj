ddb46fec9152e329b07a5259f61bc9987a0a9bb4
Егор Титенков <egortitenkov@MacBook-Air-Egor.local>
2022-10-18 17:38:02 +0300

    Initial commit

---
+++ mysite/news/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
+from .models import News
+
+
+def index(request):
+    news = News.objects.all()
+    context = {
+        'news': news,
+        'title': 'News List'
+    }
+    return render(request, 'news/index.html', context)
